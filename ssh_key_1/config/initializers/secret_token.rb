0b1813e05c24f42af9a3fd52a0db073f9dc63d1b
Sateesh Desire To Learn Heroku <sateesh.k09@gmail.com>
2012-06-02 11:29:41 +0530

    first commit

---
+++ ssh_key_1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SshKey1::Application.config.secret_token = 'a4f6188900dd3a0675b77f2ffe3a8add953fdeccc2ec7e1c2052726350530c72a59cb80dcc1f2f9edb514f372f6fbc4aabd93ca2b1b487475e9c87ad047735e0'
